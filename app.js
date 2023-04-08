a534e77ad955a936231739da7557702299e05a06
Ho Quang Lam <21521049@gm.uit.edu.vn>
2023-04-08 23:29:43 +0700

    upadte logger

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
 	credentials: true,
 	origin: ["http://localhost:3000"],
 }));
-app.use(logger())
+app.use(logger("dev"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
