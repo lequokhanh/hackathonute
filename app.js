4ebf9333f020ad13454308a1306216bd937f04da
Ho Quang Lam <21521049@gm.uit.edu.vn>
2023-04-08 22:25:33 +0700

    fix cors

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 const cors = require("cors");
 const api = require("./src/api");
 require("dotenv").config();
+const logger = require("morgan");
 
 const port = process.env.PORT || 5000;
+app.use(cors({
+	credentials: true,
+	origin: ["http://localhost:3000"],
+}));
+app.use(logger())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
-const corsConfig = {
-	credentials: true,
-	origin: ["http://localhost:3000"],
-};
-
-app.use(cors(corsConfig));
 app.use((error, req, res, next) => {
 	let { statusCode, message } = error;
 
